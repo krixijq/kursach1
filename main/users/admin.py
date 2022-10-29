711315201da97cfd3a2c09e814b2ee3903b0b4a9
YukioNice <mh.dania2015@gmail.com>
2022-10-29 15:37:27 +0500

    1

---
+++ main/users/admin.py
@@ -0,0 +1,15 @@
+from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+
+from .forms import CustomUserCreationFrom, CustomUserChangeForm
+from .models import CustomUser
+
+
+class CustomUserAdmin(UserAdmin):
+    add_form = CustomUserCreationFrom
+    form = CustomUserChangeForm
+    list_display = ['email', 'username', 'age']
+    model = CustomUser
+
+
+admin.site.register(CustomUser, CustomUserAdmin)
